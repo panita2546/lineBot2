2bcd015b37a95235792f663853d5e176688fb7ce
panita2546 <51729215+panita2546@users.noreply.github.com>
2019-08-07 09:02:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,13 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
+    sendText(user,userText)
     if (userText == 'สวัสดี') :
-        sendText('ว่าไงจ๊ะ')
+       sendText('ว่าไงจ๊ะ')
     elif (userText == 'ทำไรอยู่') :
-        sendText('เรื่องของฉัน')
+       sendText('เรื่องของฉัน')
     else :
-        sendText('ไม่เข้าใจแล้วไม่คุยด้วยแล้ว')
+       sendText('ไม่เข้าใจแล้วไม่คุยด้วยแล้ว')
 
     return '',200
 
